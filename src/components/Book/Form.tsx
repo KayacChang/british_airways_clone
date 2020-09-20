ee6993d5e0365a9038fac7a50ed9ec9b2204e50d
kayac <egg734631@gmail.com>
2020-09-20 14:17:08 +0800

    add rwd to book components

--- src/components/Book/Form.tsx
+++ src/components/Book/Form.tsx
@@ -3,6 +3,7 @@ import TextField from "./TextField";
 import styles from "./Form.module.scss";
 import { VscAccount as Account } from "react-icons/vsc";
 import { IoIosSearch as SearchIcon } from "react-icons/io";
+import { MdFlight as Flight } from "react-icons/md";
 import clsx from "clsx";
 import Button from "./Button";
 
@@ -26,49 +27,57 @@ function Trigger({ children, className }: Props) {
 }
 
 export default function Form() {
+  const size = 24;
+
   return (
     <form className={styles.form}>
       <Title />
 
-      <TextField label={"From"} hint={"type 3 letters"} />
-      <TextField label={"To"} hint={"type 3 letters"} />
+      <div className={styles.rwd}>
+        <div className={styles.vertical}>
+          <TextField label={"From"} hint={"type 3 letters"} />
+          <TextField label={"To"} hint={"type 3 letters"} />
+        </div>
 
-      <div className={styles.group}>
-        <Trigger className={styles.vertical}>
-          <span>Outbound</span>
-          <span className={styles.bigger}>19</span>
-          <span>September</span>
-        </Trigger>
+        <div className={styles.rwd_group}>
+          <Trigger className={styles.vertical}>
+            <span>Outbound</span>
+            <span className={styles.bigger}>19</span>
+            <span>September</span>
+          </Trigger>
 
-        <Trigger className={styles.vertical}>
-          <span>Add a return</span>
-          <span className={styles.bigger}>+</span>
-        </Trigger>
-      </div>
+          <Trigger className={styles.vertical}>
+            <span>Add a return</span>
+            <span className={styles.bigger}>+</span>
+          </Trigger>
+        </div>
 
-      <div className={styles.group}>
-        <Trigger className={styles.horizontal}>
-          <Account />
-          <div className={styles.vertical}>
-            <span>Passengers</span>
-            <span>1 Adult</span>
+        <div className={styles.mid_group}>
+          <div className={styles.group}>
+            <Trigger className={styles.horizontal}>
+              <Account size={size} />
+              <div className={styles.vertical}>
+                <span>Passengers</span>
+                <span>1 Adult</span>
+              </div>
+              <span>{"▾"}</span>
+            </Trigger>
+
+            <Trigger className={styles.horizontal}>
+              <Flight size={size} />
+              <div className={styles.vertical}>
+                <span>Travel class</span>
+                <span>Economy</span>
+              </div>
+              <span>{"▾"}</span>
+            </Trigger>
           </div>
-          <span>{"▾"}</span>
-        </Trigger>
 
-        <Trigger className={styles.horizontal}>
-          <Account />
-          <div className={styles.vertical}>
-            <span>Travel class</span>
-            <span>Economy</span>
-          </div>
-          <span>{"▾"}</span>
-        </Trigger>
+          <Button className={styles.search}>
+            <SearchIcon size={32} />
+          </Button>
+        </div>
       </div>
-
-      <Button className={styles.search}>
-        <SearchIcon size={32} />
-      </Button>
     </form>
   );
 }
