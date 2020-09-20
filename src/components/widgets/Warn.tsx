0f1abe8806dc5d11e6b01665298c0c2ba2358ab8
kayac <egg734631@gmail.com>
2020-09-20 16:41:06 +0800

    add warn components

--- src/components/widgets/Warn.tsx
+++ src/components/widgets/Warn.tsx
@@ -1,20 +1,45 @@
-import React from "react";
+import clsx from "clsx";
+import useDevice from "hooks/useDevice";
+import React, { ReactNode } from "react";
 import { BsInfoCircle as Info } from "react-icons/bs";
 import styles from "./Warn.module.scss";
 
-export default function Warn() {
+type Props = {
+  body?: ReactNode;
+  footer?: ReactNode;
+};
+function Mobile({ body, footer }: Props) {
   return (
-    <div className={styles.warn}>
-      <div className={styles.body}>
+    <div className={clsx(styles.warn, styles.mobile)}>
+      <div>
         <Info size={42} />
-        <h5>
-          Coronavirus | Travel and service updates and the latest advice on
-          Portugal and other countries.
-        </h5>
+        {body}
       </div>
-      <div className={styles.footer}>
-        <a href="/">Find out more</a>
+      <div>{footer}</div>
+    </div>
+  );
+}
+
+function Desktop({ body, footer }: Props) {
+  return (
+    <div className={clsx(styles.warn, styles.desktop)}>
+      <Info size={42} />
+      <div>
+        {body}
+        {footer}
       </div>
     </div>
+  );
+}
+
+export default function Warn() {
+  const text = `Coronavirus | Travel and service updates and the latest advice on Portugal and other countries.`;
+  const footer = `Find out more`;
+  const isMobile = useDevice("mobile");
+
+  return isMobile ? (
+    <Mobile body={<h5>{text}</h5>} footer={<a href="/">{footer}</a>} />
+  ) : (
+    <Desktop body={<h5>{text}</h5>} footer={<a href="/">{footer}</a>} />
   );
 }
