75800e56f9088eeb7a74c6200246c957e2a88cb1
kayac <egg734631@gmail.com>
2020-09-22 13:12:13 +0800

    add grid components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,40 @@ import Book from "components/Book";
 import ToolBar from "components/ToolBar";
 import styles from "./Home.module.scss";
 import { Warn, Login } from "components/widgets";
+import Grid from "components/widgets/Grid";
+
+function Service() {
+  const HOST = `https://www.britishairways.com`;
+
+  const list = [
+    {
+      title: "All-inclusive offers",
+      img:
+        HOST +
+        "/assets/images/MediaHub/Media-Database/Royalty-free-RF/Holidays/Beach-Holidays/666361340_600x337.jpg",
+    },
+    {
+      title: "Last-minute deals",
+      img:
+        HOST +
+        "/assets/images/MediaHub/Media-Database/Royalty-free-RF/Destinations/Netherlands/Amsterdam-AMS/470121156_337x337.jpg",
+    },
+    {
+      title: "2021 trips",
+      img:
+        HOST +
+        "/assets/images/MediaHub/Media-Database/Royalty-free-RF/Destinations/UAE/Dubai-DXB/1031039602_337x337.jpg",
+    },
+    {
+      title: "Sale",
+      img:
+        HOST +
+        "/assets/images/MediaHub/Media-Database/Rights-managed-RM/artwork/Sale/ba-jan-sale-email-header-v2_337x337.jpg",
+    },
+  ];
+
+  return <Grid list={list} />;
+}
 
 export default function Home() {
   return (
@@ -17,6 +51,7 @@ export default function Home() {
 
         <div>
           <Login />
+          <Service />
         </div>
       </div>
     </main>
